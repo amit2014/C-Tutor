80c2d2ac3d504eb6354bccf36802b8e995f172f2
Amit B Jadhav <jadhavamitbapurao@gmail.com>
2016-06-10 10:25:17 +0530

    First Program

---
+++ Helloworld/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: admin
+ *
+ * Created on 14 May, 2016, 12:23 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Hello world";
+    return 0;
+}
+
